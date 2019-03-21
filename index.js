a66d32adda0e5cb256e482958e877c13155c94de
ivanadebora <ivanadebora.wuwung@gmail.com>
2019-03-21 13:32:37 +0700

    belum bisa upload file

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { check, validationResult } = require('express-validator/check'); //form validation
-const { matchedData, sanitize } = require('express-validator/filter'); //sanitize form params
-const multer  = require('multer'); //multipar form-data
-
-const path = require('path');
-const crypto = require('crypto');
+// const { matchedData, sanitize } = require('express-validator/filter'); //sanitize form params
+// const multer  = require('multer'); //multipar form-data
+// const path = require('path');
+// const crypto = require('crypto');
 const book = require('./models/book');
 
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.static('public'))
 
 const port = process.env.PORT || 2000;
 
-const uploadDir = '/img/';
-const storage = multer.diskStorage({
-    destination: "./public"+uploadDir,
-    filename: function (req, file, cb) {
-      crypto.pseudoRandomBytes(16, function (err, raw) {
-        if (err) return cb(err)  
+// const uploadDir = '/img/';
+// const storage = multer.diskStorage({
+//     destination: "./public"+uploadDir,
+//     filename: function (req, file, cb) {
+//       crypto.pseudoRandomBytes(16, function (err, raw) {
+//         if (err) return cb(err)  
 
-        cb(null, raw.toString('hex') + path.extname(file.originalname))
-      })
-    }
-})
+//         cb(null, raw.toString('hex') + path.extname(file.originalname))
+//       })
+//     }
+// })
 
-const upload = multer({storage: storage, dest: uploadDir });
+// const upload = multer({storage: storage, dest: uploadDir });
 
 app.get('/', (req,res) => {
     res.send('<h1>Selamat datang di API!</h1>')
@@ -42,10 +41,11 @@ app.get('/book', (req, res) => {
 
 app.post('/postbook', [
     //File upload (karena pakai multer, tempatkan di posisi pertama agar membaca multipar form-data)
-    upload.single('image'),
+    // upload.single('image'),
 
     //Set form validation rule
-    
+    check('image')
+        .isLength({min:2}),
     check('name')
         .isLength({min: 2}),
     check('isbn')
@@ -79,7 +79,8 @@ app.post('/postbook', [
         year: req.body.year,
         author: req.body.author,
         description: req.body.description,
-        image: req.file === undefined ? "" : req.file.filename
+        // image: req.file === undefined ? "" : req.file.filename
+        image: req.body.image
     }).then(newBook => {
         res.json({
             "status":"success",
